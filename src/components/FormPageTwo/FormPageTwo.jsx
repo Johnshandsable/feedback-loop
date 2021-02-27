450dab7428967bec55470e2e6a93564685fdff03
Johnshandsable <Jshands@asu.edu>
2021-02-26 18:10:01 -0600

    added dispatches to each of the pages

--- src/components/FormPageTwo/FormPageTwo.jsx
+++ src/components/FormPageTwo/FormPageTwo.jsx
@@ -1,18 +1,38 @@
 import NumericInput from 'react-numeric-input';
 import { Link, useHistory } from 'react-router-dom';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function FormPageTwo() {
   const history = useHistory();
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => {
+  // Represents a number 1 to 5 for the question of how well someone is understanding the content
+  const [understandingNum, setUnderstandingNum] = useState(0);
+
+  const handleSubmit = (evt) => {
     evt.preventDefault();
+
+    dispatch({
+      type: 'SET_UNDERSTANDING',
+      payload: {
+        understanding: understandingNum,
+        page: 2,
+      },
+    });
+    // Go ahead to page three of form if the submit went ok!
+    history.push('/form3');
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <h3>How well are you understanding the content?</h3>
-        <NumericInput min={0} max={5} />
+        <NumericInput
+          min={0}
+          max={5}
+          onChange={(event) => setUnderstandingNum(event)}
+        />
         <button>Submit</button>
       </form>
       <Link to="/">Back</Link>
