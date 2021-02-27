62806ff7ff5c6531f1bb295ac5e68c5fdc487cce
Johnshandsable <Jshands@asu.edu>
2021-02-26 18:15:42 -0600

    added changes to form page five

--- src/components/FormPageFive/FormPageFive.jsx
+++ src/components/FormPageFive/FormPageFive.jsx
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function FormPageFive() {
+  const getCustomerInfo = useSelector((store) => {
+    return store.customerInfoReducer;
+  });
+
   return (
     <div>
       <h3>Thank you!</h3>
